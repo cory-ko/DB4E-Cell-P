6008b0efc1193acedff695a416efb216e512d9c1
soh-i <soh.ishiguro3914@gmail.com>
2013-09-08 16:18:01 +0900

    Fixed indent

--- parse_gbk.py
+++ parse_gbk.py
@@ -24,14 +24,14 @@ def main():
             if feature.type == 'CDS' or feature.type == 'rRNA' or feature.type == 'tRNA':
             
                 if not 'pseudo' in feature.qualifiers and 'gene' in feature.qualifiers:
-                    gene_name = feature.qualifiers['gene'][0]
-                    start = feature.location.start
-                    end = feature.location.end
-                    strand = int(feature.location.strand)
-                    seq = record.seq[start:end]
+                    gene    = feature.qualifiers['gene'][0]
+                    start   = feature.location.start
+                    end     = feature.location.end
+                    strand  = int(feature.location.strand)
+                    seq     = record.seq[start:end]
                     feature = feature.type
                     
-                    print "%s\t%s\t%s\t%s\t%s\t%s" % (gene_name, strand, start, end, feature, seq)
+                    print "%s\t%s\t%s\t%s\t%s\t%s" % (gene, strand, start, end, feature, seq)
                 
                     gene_count += 1
                 
