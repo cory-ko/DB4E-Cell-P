aa59c5733cb9b4a06f402969229cbdd6ad75e653
cory (Kazuki Oshita) <cory@g-language.org>
2013-09-08 16:24:02 +0900

    modify some mistake comments

--- bin/registration4db.py
+++ bin/registration4db.py
@@ -20,7 +20,7 @@ metadata = MetaData()
 # for debugging, please set echo=True
 engine = create_engine('sqlite:///db/ecell.db')
 
-# create table (gene)
+# table definition (Species table)
 species_table = Table('species', metadata,
                       Column('id',       Integer, primary_key=True),
                       Column('name',     String),
@@ -32,15 +32,17 @@ species_table = Table('species', metadata,
                       sqlite_autoincrement=True
                   )
 
-# create table
+# create table to DB
 metadata.create_all(engine)
 
 # mapping python class to table
 mapper(Species, species_table)
 
-# session start
+# create session object
 Session = sessionmaker()
 Session.configure(bind=engine)
+
+# session start!
 session = Session()
 
 # data registration to sqlite3
