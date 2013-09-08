5373ee6cc85ee0f85318a0c45ec768c7465392bd
coela <coela.st@gmail.com>
2013-09-08 16:30:39 +0900

    changed tabs, added info

--- misc/check_regulon_and_refseq_version.py
+++ misc/check_regulon_and_refseq_version.py
@@ -1,3 +1,6 @@
+# author : Satoshi Tamaki (coela@g-language.org)
+# SYNOPSIS : python check_regulon_and_refseq_version.py promoter.txt NC_000913.gbk | grep -c False
+
 import sys
 import re
 from Bio import SeqIO
@@ -7,12 +10,13 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 gb_record = SeqIO.read(sys.argv[2], "genbank")
 
 for line in open(sys.argv[1], 'r'):
-		line = line.rstrip()
-		if not re.match('^[#]',line):
-				lineArray = line.split("\t")
-				start = int(lineArray[3])
-				string = lineArray[9]
-				if lineArray[2] in "forward": 
-						print (string.lower() ) in gb_record.seq[start - 61 : start + 20].lower()
-				else:
-						print string.lower() in gb_record.seq[start - 21 : start+60].reverse_complement().lower()
+    line = line.rstrip()
+    if not re.match('^[#]',line):
+        lineArray = line.split("\t")
+        start = int(lineArray[3])
+        string = lineArray[9]
+        
+        if lineArray[2] in "forward": 
+            print (string.lower() ) in gb_record.seq[start - 61 : start + 20].lower()
+        else:
+            print string.lower() in gb_record.seq[start - 21 : start + 60].reverse_complement().lower()
