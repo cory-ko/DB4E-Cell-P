dbd515111f6e813ab55099e1bee6a7ae569bc1ac
soh-i <soh.ishiguro3914@gmail.com>
2013-09-10 01:33:54 +0900

    nemui

--- bin/coordinate.py
+++ bin/coordinate.py
@@ -8,11 +8,21 @@ Name: Soh Ishiguro
 E-mail: t10078si@sfc.keio.ac.jp
 '''
 
-def extract_seq(seq='', start='', end=''):
+class GenomicCoordinate(object):
+
+    def __init__(start, end, seq, circular):
+        self.start = start
+        self.end = end
+        self.seq = seq
+        self.circular = circular
+
+    
+def extract_seq(seq='', start='', end='', circular=True):
+    
     '''
     Genomic coordinate(1-based)
     
-    Argument :    sequence, start, end
+    Argument :    sequence, start, end, circular(default:True)
     Return value: extracted sequence
     
     '''
@@ -22,7 +32,41 @@ def extract_seq(seq='', start='', end=''):
 
     length = len(seq)
     print seq
+
+    def circular():
+        
+        '''
+        For circular genomic coordinate
+        '''
+
+        if start > 0 and end > 0:
+            return seq[start-1:end]
+
+        elif start > 0 and end < 0:
+            return seq[start-1:length-abs(end)]
+            
+
+    def linear():
+        
+        '''
+        For lnear genomic coordinate.
+        '''
+        
+        if start > 0 and end > 0:
+            pass
+
+        elif start > 0 and end < 0:
+            pass
     
+    if circular:
+        print "Circular genome sequence: "
+        print circular()
+        
+    elif not circular:
+        print "Linear sequence: "
+        print linear()
+        
+"""
     if start > 0 and end > 0:
         return seq[start-1:end]
         
@@ -37,15 +81,16 @@ def extract_seq(seq='', start='', end=''):
         
     elif start == 0 or end == 0:
         pass
-        
-        
+
+"""
+
 
 def main():
     seq = 'TCATTTAGCCTCTCCACTTTGAGCCTGCTGAAACAAGGTG' #40
-    s = 1
-    e = 3
+    s = 2
+    e = -3
     
-    print extract_seq(seq=seq, start=s, end=e)
+    extract_seq(seq=seq, start=s, end=e, circular=True)
 
     
 if __name__ == '__main__':
