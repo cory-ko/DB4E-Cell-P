e70b8095ddbb5e8676b36225e78b58e8a88a6b7e
coela <coela.st@gmail.com>
2013-09-09 12:05:36 +0900

    added documnts

--- bin/generate_promoter.py
+++ bin/generate_promoter.py
@@ -2,16 +2,32 @@
 
 import sys
 import re
+__author__ = """
+Name: Satoshi Tamaki
+E-mail: coela.st@gmail.com
+"""
 
-for line in open(sys.argv[1], 'r'):
-    line = line.rstrip()
-    if not re.match('^[#]',line):
-        lineArray = line.split("\t")
+def main():
+    """
+This script generates the operon DB from promoter.txt,
+input data was collected from web resource of the RegulonDB.(not from Web page)
+SYNOPSIS: python generate_promoter.py promoter.txt
+OUTPUT : promoter_id strand start end type(promoter) sequence
+    """
 
-        if not len(lineArray[3]):
-            continue
-        if lineArray[2] in "forward":
-            print "\t".join([ lineArray[0],"1",lineArray[3],lineArray[3],"promoter",lineArray[9] ])
-        else:
-            print "\t".join([ lineArray[0],"-1",lineArray[3],lineArray[3],"promoter",lineArray[9] ])
+    for line in open(sys.argv[1], 'r'):
+        line = line.rstrip()
+        if not re.match('^[#]',line):
+            lineArray = line.split("\t")
 
+            if not len(lineArray[3]):
+                continue
+
+            if lineArray[2] in "forward":
+                print "\t".join([ lineArray[0],"1",lineArray[3],lineArray[3],"promoter",lineArray[9] ])
+            else:
+                print "\t".join([ lineArray[0],"-1",lineArray[3],lineArray[3],"promoter",lineArray[9] ])
+
+
+if __name__ == '__main__':
+    main()
