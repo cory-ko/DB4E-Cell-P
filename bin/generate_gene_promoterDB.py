9cbbe3dc2d0302f5b6dccb01881f5936d5ca0b91
soh-i <soh.ishiguro3914@gmail.com>
2013-09-09 11:31:30 +0900

    Initial commit

---
+++ bin/generate_gene_promoterDB.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys
+from Bio import SeqIO
+import pprint
+
+__author__ = '''
+Name: Soh Ishiguro
+E-mail: t10078si@sfc.kei.ac.jp
+'''
+
+def main():
+    
+    file = '/home/soh.i/E-cell_Sprint/RegulonDATADist/OperonSet.txt'
+
+    DEBUG = 0
+
+    integrated_data = {}
+    with open(file, 'r') as f:
+        for line in f:
+        
+            if not line.startswith("#") and not line.isspace():
+                data = line[:-1].split("\t")
+            
+                operon_name = 'OPERON_' + data[0]
+                start       = int(data[1])
+                end         = int(data[2])
+                strand      = data[3]
+                num_operon  = int(data[4])
+                gene_names  = data[5]
+            
+                if 'forward' in strand:
+                    strand = int(1)
+                elif 'reverse' in strand:
+                    strand = int(-1)
+                
+                gene = gene_names.split(",")
+                for g in gene:
+                    integrated_data.update({g : {'operon_id' : operon_name, 'strand' : strand}})
+
+
+    if not DEBUG:
+        for k in integrated_data:
+            print "%s\t%d\t%s" % (k, integrated_data[k]['strand'], integrated_data[k]['operon_id'])
+    else:
+        pp = pprint.PrettyPrinter(depth=6)
+        pp.pprint(integrate)
+        
+    
+if __name__ == '__main__':
+    main()
+    
+                
+
