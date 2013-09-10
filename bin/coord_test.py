70283bd923fb01a8fc51a37d35dc7431a2e11a9e
soh-i <soh.ishiguro3914@gmail.com>
2013-09-11 00:54:48 +0900

    add test code

--- bin/coord_test.py
+++ bin/coord_test.py
@@ -3,4 +3,16 @@
 from coordinate import GenomicCoordinate
 from coordinate import GenomicArray
 
-import unittest
+import pytest
+
+def test_genomic_coordinate():
+    
+    ecoli_genome = '/home/soh.i/E-cell_Sprint/NC_000913.fna'
+    ga = GenomicArray(ecoli_genome)	
+    whole_seq_array = ga.seq_to_array()
+
+    gc = GenomicCoordinate(start=30, end=200, seq=whole_seq_array, circular=True)
+    assert gc.retrieve_seq() == "ATGC"
+
+test_genomic_coordinate()
+    
