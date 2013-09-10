f9d64cfe3d4db62e5bd7e571db42c2ff07ff673c
soh-i <soh.ishiguro3914@gmail.com>
2013-09-10 12:52:36 +0900

    add attibute to genomic arrary

--- bin/generate_genomic_array.py
+++ bin/generate_genomic_array.py
@@ -5,11 +5,25 @@ import string
 
 from Bio import SeqIO
 
-def to_array(seq):
+def seq_to_array(seq):
     record = SeqIO.read(seq, "fasta")
     return [s for s in record]
+
+def add_attribute_to_genomic_array():
+    pass
+
     
 ecoli_genome = '/home/soh.i/E-cell_Sprint/NC_000913.fna'
-print to_array(ecoli_genome)
+annotation_file = ''
+
+with open(culated_file,'r') as f:
+    for line in f:
+        (gene, gene_start, gene_end, gene_strand) = line[:-1].split("\t")
+        attribute = { gene: { "gene" : gene, "start" : start, "end" : end}}
+
+        genomic_array = seq_to_array(ecoli_genome)
+        for index, base enumerate in genomic_array:
+            if gene_start <= base and gene_end >= base:
+                genomic_array[index].append(attribute)
 
 
