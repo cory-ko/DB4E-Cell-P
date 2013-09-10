22c6e665214f43b44db6d854f5c36011931a244b
soh-i <soh.ishiguro3914@gmail.com>
2013-09-10 16:35:51 +0900

    add generic class

--- bin/coordinate.py
+++ bin/coordinate.py
@@ -15,23 +15,52 @@ class GenomicCoordinate(object):
         self.end = end
         self.seq = seq
         self.circular = circular
+        self.length = len(seq)
 
-    
+    def __circular(self):
+        pass
+
+    def __linear(self):
+        
+        '''
+        For lnear genomic coordinate.
+        '''
+        
+        if self.start > 0 and self.end > 0:
+            return self.seq[self.start:self.seq]
+
+        elif self.start > 0 and end > self.length:
+            return self.seq[self.start:self.length]
+            
+        elif self.start > self.length:
+            return ''
+
+            
+    def retrieve_seq(self):
+        if self.circular:
+            __circular()
+            
+        elif not self.circular:
+            __linear()
+
+
+
+        
 def extract_seq(seq='', start='', end='', circular=True):
     
     '''
-    Genomic coordinate(1-based)
+    Genomic coordinate(1-based).
     
     Argument :    sequence, start, end, circular(default:True)
     Return value: extracted sequence
     
     '''
     
-    if not seq or not start or not end:
-        raise ValueError
+    if not seq or not start or not end or start == 0 or end == 0:
+        raise ValueError("Invalid data")
 
     length = len(seq)
-    print seq
+    print "Original seq: " + seq
 
     
     def circular():
@@ -40,7 +69,13 @@ def extract_seq(seq='', start='', end='', circular=True):
         For circular genomic coordinate
         '''
 
-        if start > 0 and end > 0:
+        if start > end:
+            return seq[start-1:end]
+
+        elif end < start:
+            return seq[start:length % end ]
+            
+        elif start > 0 and end > 0:
             return seq[start-1:end]
 
         elif start > 0 and end < 0:
@@ -51,10 +86,8 @@ def extract_seq(seq='', start='', end='', circular=True):
             
         elif start < 0 and end < 0:
             return seq[length-abs(end):length-abs(start)]
-        
-            
-            
 
+        
     def linear():
         
         '''
@@ -62,11 +95,14 @@ def extract_seq(seq='', start='', end='', circular=True):
         '''
         
         if start > 0 and end > 0:
-            pass
+            return seq[start:seq]
 
-        elif start > 0 and end < 0:
-            pass
-    
+        elif start > 0 and end > length:
+            return seq[start:length]
+            
+        elif start > length:
+            return ''
+                
     if circular:
         print "Circular genome sequence: ",
         print circular()
@@ -75,32 +111,14 @@ def extract_seq(seq='', start='', end='', circular=True):
         print "Linear sequence: ",
         print linear()
         
-"""
-    if start > 0 and end > 0:
-        return seq[start-1:end]
-        
-    elif start > 0 and end < 0:
-        pass
-        
-    elif start < 0 and end > 0:
-        pass
-        
-    elif start < 0 and end < 0:
-        pass
-        
-    elif start == 0 or end == 0:
-        pass
-
-"""
-
 
 def main():
     seq = 'TCATTTAGCCTCTCCACTTTGAGCCTGCTGAAACAAGGTG' #40
-    s = -1
-    e = -10
+    s =  30
+    e =  100
     
     print "start: %d, end: %d" %(s, e)
-    extract_seq(seq=seq, start=s, end=e, circular=True)
+    extract_seq(seq=seq, start=s, end=e, circular=False)
 
     
 if __name__ == '__main__':
