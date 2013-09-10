99fbe4ffa02cc1420564b2dd6f2e88013e400ba6
coela <coela.st@gmail.com>
2013-09-11 04:35:09 +0900

    works from class now

--- db4ecellp/registration/registration4db.py
+++ db4ecellp/registration/registration4db.py
@@ -3,54 +3,53 @@
 __author__ = 'Kazuki Oshita <cory@g-language.org>'
 
 import sys
-sys.path.append('./lib')
 
-from species import *
+from species import species
 
-# for debugging, please set echo=True
-engine = create_engine('sqlite:///:memory:')
+def generate_db():
+    # for debugging, please set echo=True
+    engine = species.create_engine('sqlite:///:memory:',echo=True)
 
-# create table to DB
-metadata.create_all(engine)
+    # create table to DB
+    species.metadata.create_all(engine)
 
-# create session object
-Session = sessionmaker()
-Session.configure(bind=engine)
+    # create session object
+    Session = species.sessionmaker()
+    Session.configure(bind=engine)
 
-# session start!
-session = Session()
+    # session start!
+    session = Session()
 
-# data registration to sqlite3
+    # data registration to sqlite3
 
-with open("data/CDS_annotation.tbl", "r") as f:
-    for line in f:
-        (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
-        obj = CDS(name, strand, start, end, feature, sequence)
-        session.add(obj)
+    with open("data/CDS_annotation.tbl", "r") as f:
+        for line in f:
+            (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
+            obj = species.CDS(name, strand, start, end, feature, sequence)
+            session.add(obj)
 
-with open("data/rRNA_annotation.tbl", "r") as f:
-    for line in f:
-        (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
-        obj = rRNA(name, strand, start, end, feature, sequence)
-        session.add(obj)
+    with open("data/rRNA_annotation.tbl", "r") as f:
+        for line in f:
+            (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
+            obj = species.rRNA(name, strand, start, end, feature, sequence)
+            session.add(obj)
 
-with open("data/tRNA_annotation.tbl", "r") as f:
-    for line in f:
-        (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
-        obj = tRNA(name, strand, start, end, feature, sequence)
-        session.add(obj)
-with open("data/promoter_annotation.tbl", "r") as f:
-    for line in f:
-        (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
-        obj = tRNA(name, strand, start, end, feature, sequence)
-        session.add(obj)
+    with open("data/tRNA_annotation.tbl", "r") as f:
+        for line in f:
+            (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
+            obj = species.tRNA(name, strand, start, end, feature, sequence)
+            session.add(obj)
 
-with open("data/terminator_annotation.tbl", "r") as f:
-    for line in f:
-        (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
-        obj = tRNA(name, strand, start, end, feature, sequence)
-        session.add(obj)
+    with open("data/promoter_annotation.tbl", "r") as f:
+        for line in f:
+            (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
+            obj = species.Promoter(name, strand, start, end, feature, sequence)
+            session.add(obj)
 
+    with open("data/terminator_annotation.tbl", "r") as f:
+        for line in f:
+            (name, strand, start, end, feature, sequence) = line[:-1].split("\t")
+            obj = species.Terminator(name, strand, start, end, feature, sequence)
+            session.add(obj)
 
-# commit to database
-session.commit()
+    session.commit()
