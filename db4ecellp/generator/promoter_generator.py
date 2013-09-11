058a1ea1eec13d8df1dc7f9d16d4103f259c5df6
soh-i <soh.ishiguro3914@gmail.com>
2013-09-11 13:12:37 +0900

    Fixed IndexError

--- db4ecellp/generator/promoter_generator.py
+++ db4ecellp/generator/promoter_generator.py
@@ -2,6 +2,8 @@
 
 import sys
 import re
+import string
+
 __author__ = """
 Name: Satoshi Tamaki
 E-mail: coela.st@gmail.com
@@ -19,18 +21,22 @@ def generate_annotation_files(promoter_file):
     output_file = open('data/promoter_annotation.tbl','w');
 
     for line in open(promoter_file, 'r'):
-        line = line.rstrip()
-        if not re.match('^[#]',line):
-            lineArray = line.split("\t")
+       if (line.isspace()):
+            continue
 
-            if not len(lineArray[3]):
+       line = line.rstrip()
+       if not re.match('^#',line):
+            lineArray = line.split("\t")
+            
+            if not len(lineArray) == 7 :
                 continue
 
+            print lineArray
             if lineArray[2] in "forward":
-                text = "\t".join([ lineArray[0],"1",lineArray[3],lineArray[3],"promoter",lineArray[9] ])
+                text = "\t".join([ lineArray[0],"1",lineArray[3],lineArray[3],"promoter",lineArray[5] ])
                 output_file.write(text + "\n")
 
             else:
-                text =  "\t".join([ lineArray[0],"-1",lineArray[3],lineArray[3],"promoter",lineArray[9] ])
+                text =  "\t".join([ lineArray[0],"-1",lineArray[3],lineArray[3],"promoter",lineArray[5] ])
                 output_file.write(text + "\n")
 
