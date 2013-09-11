c3289d59794e3f0d3d52e1d2d5d646ca19568f47
soh-i <soh.ishiguro3914@gmail.com>
2013-09-11 11:51:42 +0900

    Wrappng class and added run() function

--- demo.py
+++ demo.py
@@ -1,17 +1,29 @@
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python
 
-import os, sys
+import os
+import sys
 
 path = os.path.abspath(os.path.dirname(__file__)) + '/..'
 sys.path.append(path)
 
 from db4ecellp import db4ecellp
 
-def main():
-    _db4ecellp = db4ecellp.Db4ecellp()
-    _db4ecellp.generate_files()
-    _db4ecellp.generate_db()
+class DB4EcellpDemo(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        self.db4ecellp = db4ecellp.Db4ecellp()
+
+        print "Generating files..."
+        self.db4ecellp.generate_files()
+
+        print "Gennerating DB..."
+        self.db4ecellp.generate_db()
     
 if __name__ == "__main__":
-    main()
+    
+    print "Running the DB4 E-Cell P...."
+    demo = DB4EcellpDemo()
+    demo.run()
 
